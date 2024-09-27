88afa06e67e1ab2d6286eeb5924ebef243473d72
Nadezh_da <mamonova777@mail.ru>
2024-09-27 16:51:08 +0300

    Initials commit

--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 
 from books.views import books_view
+import phones.views
 
 urlpatterns = [
     path('', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('', phones.views.index),
+    path('catalog/', phones.views.show_catalog, name='catalog'),
+    path('catalog/<slug:slug>/', phones.views.show_product, name='phone'),
 ]
